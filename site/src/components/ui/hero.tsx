29a149d0c3ca6e7c20cfe528af418b11e8a51e3b
Jayden Priestley <jaydenUX@outlook.com>
2025-05-18 00:30:41 +0100

    changed hero text

--- site/src/components/ui/hero.tsx
+++ site/src/components/ui/hero.tsx
@@ -1,17 +1,12 @@
-import { Instrument_Serif } from "next/font/google";
-
-const instrument = Instrument_Serif({
-    weight: "400",
-    style: "normal",
-    subsets: ["latin"],
-});
-
 export default function Hero() {
     return (
-        <div className="text-center py-16 mb-16 border-b border-b-border">
-            <h1 className={`${instrument.className} text-6xl text-foreground`}>
-                An unprofessional opinion on the best music.
+        <div className="text-center font-serif py-16 mb-16 border-b border-b-border">
+            <h1 className="text-4xl lg:text-6xl text-foreground">
+                Judge music by the way it makes <em>you feel</em>,
             </h1>
+            <p className="text-xl lg:text-2xl">
+                not the way it makes <s>others feel</s>.
+            </p>
         </div>
     );
 }
