7cea1884dec0fea678cd6571e243353c5a24d122
Jayden Priestley <jaydenUX@outlook.com>
2025-05-26 04:54:15 +0100

    changed the client config for sanity

--- site/src/app/sanity.ts
+++ site/src/app/sanity.ts
@@ -4,7 +4,7 @@ import { createClient, type ClientConfig } from "@sanity/client";
 const config: ClientConfig = {
     projectId: "5tasc9nv",
     dataset: "production",
-    useCdn: true, // set to `false` to bypass the edge cache
+    useCdn: false, // set to `false` to bypass the edge cache
     apiVersion: "2025-02-06", // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
     // token: process.env.SANITY_SECRET_TOKEN // Needed for certain operations like updating content, accessing drafts or using draft perspectives
 };
